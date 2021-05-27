07cef413c9b1a8eb9fbbdfe2bba16d7ca985c033
LiadNahum1 <liadna@post.bgu.ac.il>
2021-05-28 00:24:56 +0300

    finish level 2

--- ex02_M2.py
+++ ex02_M2.py
@@ -1,15 +1,19 @@
-#207434044 - level1 - 536a0ffbb9f89cb807b2e3a565bc3ca2
-#318841285 - level1 - 0b323015266587b7422b4735dee4b9cc [41, 45, 49, 53, 42, 46, 50, 54, 43, 47, 51, 55, 44, 48, 52, 56]
-#318841285 - level2 - 7e680f4a466e17b864e561013eeac270 [41, 45, 49, 53, 42, 46, 50, 54, 43, 47, 51, 55, 44, 48, 52, 56]
+import sys
+import requests
+import os
 import json
 import numpy as np
+import matplotlib.pyplot as plt
 
-USER = "318841285"
-DIFFICULTY = 2
-NUMBER_OF_LEAKS = 64
+CORRECT_NUMBER_OF_ARGUMENTS = 2
+NOT_FOUND_ERROR_CODE = 404
+
 NUMBER_OF_TRACES = 10000
 KEY_LENGTH = 16
-FILE_NAME = "318841285/traces_318841285_2.txt"
+KEY_BYTE_POSSIBILITIES = 256
+CORRECT_PASSWORDS = {'318841285': '7e680f4a466e17b864e561013eeac270', '207434044': '713ba1f9173aa51729cc8086e1f02b06',
+                     '319082681': '5b717730f422759b720dc31cec0f0ce1'}
+
 AES_SBOX = [
     0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
     0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
@@ -38,12 +42,32 @@ HW = [0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4, 1, 2, 2, 3, 2, 3, 3, 4, 2,
       4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8]
 
 
+def download_power_traces(filename, server_url, number_of_power_traces):
+    """
+    This function downloads `number_of_power_traces` traces from a remote server
+    and write those traces to a local file
+    :param filename: the name of the file witch will contain the traces
+    :param server_url: the remote server url to dwonload the traces from
+    :param number_of_power_traces: the number of traces to dwonload
+    :return: this function does not return a value, it writes the traces to a file
+    """
+    leaks_list = []
+    for i in range(number_of_power_traces):
+        response = requests.get(server_url)
+        if response.status_code == NOT_FOUND_ERROR_CODE:
+            print(f'something wrong with the server, recived code: {response.status_code}, exiting...')
+            sys.exit()
+        response_as_json = response.json()
+        leaks_list.append(f'{json.dumps(response_as_json)}\n')
+    with open(filename, 'w') as traces_file:
+        traces_file.writelines(leaks_list)
+
+
 def get_traces_and_plaintexts(filename):
     """
-    returns the traces and plaintexts as numpy arrays.
-    traces is a 2-dimensional array
+    This function returns the traces and plaintexts as numpy arrays.
+    :param filename: the name of the file which contains the traces
     """
-
     with open(filename, 'r') as traces_file:
         leaks_list = traces_file.readlines()
         leaks_list = [json.loads(leak) for leak in leaks_list]
@@ -51,8 +75,7 @@ def get_traces_and_plaintexts(filename):
         plaintexts = [leaks_dict['plaintext'] for leaks_dict in leaks_list]
         plaintexts = [bytes.fromhex(plaintext) for plaintext in plaintexts]
         plaintexts = [[byte for byte in plaintext] for plaintext in plaintexts]
-
-    return np.array(traces), np.array(plaintexts)  # traces 10000X64 , plaintexts 10000X16
+    return np.array(traces), np.array(plaintexts)
 
 
 def apply_sbox(num):
@@ -63,48 +86,123 @@ def apply_hw(num):
     return HW[num]
 
 
-traces, plaintexts = get_traces_and_plaintexts(FILE_NAME)
-v_apply_sbox = np.vectorize(apply_sbox)
-v_apply_hw = np.vectorize(apply_hw)
-num_traces = len(plaintexts)
-num_of_leaks = np.shape(traces)[1]
-important_times_per_byte = []
+def byte_process(traces, plaintexts, key_byte):
+    """
+   This function searches for the correct byte of the key.
+   :param traces: traces of power consumptions
+   :param plaintexts: plaintext for each trace,  as an array of bytes. Each plaintext is 16 bytes
+   :param key_byte: the index of the key byte that the function searches
+   :return: the function finds the cell with the highest correlation and returns the correct byte (row index of the cell)
+   with the correct time point (column index of the cell).
+    """
+    num_traces = len(plaintexts)
+    num_of_leaks = np.shape(traces)[1]
 
+    # make apply_sbox and apply_hw function work on vectors
+    v_apply_sbox = np.vectorize(apply_sbox)
+    v_apply_hw = np.vectorize(apply_hw)
 
-def byte_process(key_byte, key_array):
-    global num_of_leaks, num_traces, plaintexts, v_apply_sbox, v_apply_hw, traces, important_times_per_byte
-    differences = np.zeros(shape=(256, num_of_leaks))
+    differences = np.zeros(shape=(KEY_BYTE_POSSIBILITIES, num_of_leaks))  # contains the correlations
+    hw_matrix = np.zeros(shape=(num_traces, KEY_BYTE_POSSIBILITIES))  # contains hw(sbox(p_i xor k_i))
 
-    traces_class = np.zeros(shape=(num_traces, 256))
-    for byte_guess in range(256):
-        # gives an array of the current byte we check xor-ed with the key for each plaintext
-        byte_with_key_guesses = np.bitwise_xor(plaintexts[:, key_byte], byte_guess)
-        sboxed_bytes = v_apply_sbox(byte_with_key_guesses)
-        # put in the place of the current byte guess the hw of all plaintext (on the number of byte we are guessing)
-        # after first sbox with the key guess
-        traces_class[:, byte_guess] = v_apply_hw(sboxed_bytes)
+    for byte_guess in range(KEY_BYTE_POSSIBILITIES):
+        # xor each plaintext with the guessed byte
+        xor_plaintext_with_key_byte = np.bitwise_xor(plaintexts[:, key_byte], byte_guess)
+        # apply sbox on each xor result
+        sboxed_bytes = v_apply_sbox(xor_plaintext_with_key_byte)
+        # apply hw on each sbox result. put this vector as a column in hw_matrix at column index byte_guess
+        hw_matrix[:, byte_guess] = v_apply_hw(sboxed_bytes)
 
+        # for each time point and byte guess save a correlation
         for i in range(num_of_leaks):
-            differences[byte_guess, i] = np.corrcoef(traces_class[:, byte_guess], traces[:, i])[0][1]
-
-    print(f'10000X256 {traces_class}')
-    print(f'256X64 {differences}')
+            differences[byte_guess, i] = np.corrcoef(hw_matrix[:, byte_guess], traces[:, i])[0][1]
 
     differences = np.abs(differences)
-    # take the byte of where the correlation was most powerful
-    key_array[key_byte] = (np.where(differences == np.max(differences))[0][0])
-    important_times_per_byte.append((np.where(differences == np.max(differences))[1][0]))
+    # find the cell with the highest correlation.
+    # returns the byte and the time point
+    correct_key_byte = np.where(differences == np.max(differences))[0][0]
+    correct_time = np.where(differences == np.max(differences))[1][0]
+    return correct_key_byte, correct_time
+
+
+def get_correct_key(traces, plaintexts, number_of_traces=NUMBER_OF_TRACES):
+    """
+       :param traces: traces of power consumptions
+       :param plaintexts: plaintext for each trace,  as an array of bytes. Each plaintext is 16 bytes
+       :param number_of_traces: a number specifies how many traces the function can use in order to
+       find the key.
+       :return the function returns the key and the important time points it found. 
+    """
+    key = [0] * KEY_LENGTH
+    important_times_per_byte = []
+    for key_byte in range(KEY_LENGTH):
+        key[key_byte], time = byte_process(traces[:number_of_traces, :], plaintexts[:number_of_traces, :], key_byte)
+        important_times_per_byte.append(time)
+
+    key = ''.join(list(map(lambda x: "{:02x}".format(x), key)))
+    return key, important_times_per_byte
+
+
+def check_how_many_correct_bytes(key, correct_key):
+    """
+       This function checks how many correct bytes there are in key compared to correct_key
+       :param key: a key represented as string. 32 characters are representing 16 bytes.
+       :param correct_key: a key represented as string and compare to it, the function counts how many correct bytes
+       :return: returns the number of correct bytes in key
+    """
+    correct_bytes = 0
+    for char_index in range(0, len(key), 2):
+        if key[char_index:char_index + 2] == correct_key[char_index:char_index + 2]:
+            correct_bytes += 1
+    return correct_bytes
+
+
+def correct_bytes_number_traces(traces, plaintexts, correct_key):
+    """
+       This function goes over different number of traces in step of size 1000, and checks for each number of
+       traces, how many correct bytes there are in the founded key. Eventually plots a graph with the number of
+       traces on the x scale and the count of correct bytes in the output on the y scale
+       :param traces: traces of power consumptions
+       :param plaintexts: plaintext for each trace,  as an array of bytes. Each plaintext is 16 bytes
+       :param correct_key: a key represented as string and compare to it, the function counts how many correct bytes in
+       the calculated key
+    """
+    y_number_of_correct_bytes = []
+    x_traces = range(1000, NUMBER_OF_TRACES + 1000, 1000)
+    for i in x_traces:
+        key, _ = get_correct_key(traces, plaintexts, i)
+        correct_bytes = check_how_many_correct_bytes(key, correct_key)
+        y_number_of_correct_bytes.append(correct_bytes)
+        print(y_number_of_correct_bytes)
+
+    # plot graph
+    plt.plot(x_traces, np.asarray(y_number_of_correct_bytes))
+    plt.legend()
+    plt.xlabel('Number Of Traces')
+    plt.ylabel('Number Of Correct Bytes')
+    plt.title('User 318841285 Difficulty 2')
+    plt.show()
 
 
 def main():
-    print(FILE_NAME)
-    key = [0] * KEY_LENGTH
-    for key_byte in range(KEY_LENGTH):
-        byte_process(key_byte, key)
+    user = '318841285'
+    difficulty = 2
+    server_url = f'http://aoi.ise.bgu.ac.il/encrypt?user={user}&difficulty={difficulty}'
 
-    key = ''.join(list(map(lambda x: "{:02x}".format(x), key)))
-    print(f'{USER},{key},{DIFFICULTY}')
-    print(important_times_per_byte)
+    if len(sys.argv) < CORRECT_NUMBER_OF_ARGUMENTS:
+        print(f'Illegal number of arguments')
+        sys.exit()
+
+    filename = sys.argv[1]
+    if not os.path.isfile(filename):
+        download_power_traces(filename, server_url, NUMBER_OF_TRACES)
+
+    traces, plaintexts = get_traces_and_plaintexts(filename)
+    key, important_times_per_byte = get_correct_key(traces, plaintexts)
+    print(f'{user},{key},{difficulty}')
+
+    #print(important_times_per_byte)
+    #correct_bytes_number_traces(traces, plaintexts, '7e680f4a466e17b864e561013eeac270')
 
 
 if __name__ == '__main__':
